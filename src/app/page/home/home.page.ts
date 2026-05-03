626ca1fb59c9cc5d833cf5496d819ebfe8beebb6
SimonEzosp <simonrodriguezospina5870@gmail.com>
2026-05-02 20:28:38 -0500

    implementacion de diseño

--- src/app/page/home/home.page.ts
+++ src/app/page/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonGrid,IonButton,IonCard,IonCardTitle,IonCardSubtitle,IonCardContent,IonCardHeader,IonText,IonAvatar,IonImg,IonInput,IonInputPasswordToggle  } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,IonGrid,IonButton,IonCard,IonCardTitle,IonCardSubtitle,IonCardContent,IonCardHeader,IonText,IonAvatar,IonImg,IonInput,IonInputPasswordToggle,IonRow,IonCol  } from '@ionic/angular/standalone';
 import {LoginComponent} from 'src/app/components/login/login.component'
 import {UsuarioComponent} from 'src/app/components/usuario/usuario.component'
 
@@ -10,10 +10,14 @@ import {UsuarioComponent} from 'src/app/components/usuario/usuario.component'
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonGrid,IonButton,IonCard,IonCardTitle,IonCardSubtitle,IonCardContent,IonCardHeader,IonText,IonAvatar,IonImg,IonInput,IonInputPasswordToggle,LoginComponent,UsuarioComponent  ]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonGrid,IonButton,IonCard,IonCardTitle,IonCardSubtitle,IonCardContent,IonCardHeader,IonText,IonAvatar,IonImg,IonInput,IonInputPasswordToggle,LoginComponent,UsuarioComponent,IonRow,
+    IonCol
+    ]
 })
 export class HomePage implements OnInit {
-  simon: string ="src/assets/icon/simon.jpg";
+  // home.page.ts
+    jyk = 'assets/icon/jyk.png';
+    simon = 'assets/icon/simon.png';
   constructor() { }
 
   ngOnInit() {
